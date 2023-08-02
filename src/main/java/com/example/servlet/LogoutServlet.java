0fc5eb2f03440c9bacfc4570fa6416966d3ba6c4
Ivan Artsiushyn <ivanart555@gmail.com>
2023-08-02 21:50:40 +0300

    Add logging and fix LoginServlet

--- src/main/java/com/example/servlet/LogoutServlet.java
+++ src/main/java/com/example/servlet/LogoutServlet.java
@@ -6,15 +6,22 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.logging.Logger;
 
 @WebServlet("/logout")
 public class LogoutServlet extends HttpServlet {
+    private static final Logger log = Logger.getLogger(LogoutServlet.class.getName());
+    private static final String LOGIN_JSP = "/login.jsp";
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
         HttpSession session = req.getSession();
-        session.setAttribute("user", null);
+        session.removeAttribute("user");
         session.invalidate();
-        resp.sendRedirect("/login.jsp");
+        try {
+            resp.sendRedirect(LOGIN_JSP);
+        } catch (IOException e) {
+            log.info("Failed to redirect Get request to page: " + LOGIN_JSP + " " + e.getMessage());
+        }
     }
 }
